671c4e4634a797392ccda32e67ee9e74403259ec
patxi <patxiarana05@gmail.com>
2023-12-30 12:35:56 -0300

    cargando en la db

--- bienesRaicesPOO_PHP_inicio/classes/Propiedad.php
+++ bienesRaicesPOO_PHP_inicio/classes/Propiedad.php
@@ -48,10 +48,17 @@ class Propiedad
     {
         //Sanitizar los datos 
         $atributos = $this->sanitizarAtributos();
+          
+ 
 
-        $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, vendedores_id, creado  ) VALUES ( '$this->titulo', '$this->precio', '$this->imagen',
-         '$this->descripcion',  '$this->habitaciones', '$this->wc', '$this->estacionamiento', '$this->vendedores_id', '$this->creado' )";
 
+        //Insertar en la base de datos 
+        $query = "INSERT INTO propiedades ("; 
+        $query .= join(', ',array_keys($atributos)) ; 
+        $query .= " ) VALUES (' "; 
+        $query .= join("', '",array_values($atributos)) ; 
+        $query .=  " ')";
+           
         $resultado =  self::$db->query($query);
 
         debuguear($resultado);
